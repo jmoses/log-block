8b3d03f8788aaefbdb6e26a8b4d459cff3898f45
Jon Moses <jon@burningbush.us>
2010-03-23 20:25:17 -0400

    Refactor

--- lib/log_block.rb
+++ lib/log_block.rb
@@ -1,32 +1,44 @@
 require 'logger'
 
-class Logger
-  def with( prefix, indent_size = 2 )
-    yield get_block_logger( prefix, indent_size )
-  end
-  
-  def get_block_logger( prefix, indent_size = 2)
-    BlockLogger.new( self, prefix, indent_size )
-  end
-end
-
-class BlockLogger
-  def initialize( logger, prefix, indent_size = 2 )
-    @logger = logger
-    @indent_size = indent_size
-    @prefix = prefix
-  end
-
-  def method_missing( meth, *args )
-    if [:debug, :info, :warn, :error, :fatal, :unknown].include?(meth)
-      @logger.send(meth, message(args.first))
-    else
-      super
+module LogBlock
+  module InstanceMethods
+    def with( prefix, indent_size = 2 )
+      yield get_block_logger( prefix, indent_size )
     end
+
+    def get_block_logger( prefix, indent_size = 2)
+      ::LogBlock::BlockLogger.new( self, prefix, indent_size )
+    end    
   end
-  
-  def message( msg )
-    "#{' ' * @indent_size}#{@prefix}#{msg}"
+
+  class BlockLogger
+    def initialize( logger, prefix, indent_size = 2 )
+      @logger = logger
+      @indent_size = indent_size
+      @prefix = prefix
+    end
+
+    def method_missing( meth, *args )
+      if [:debug, :info, :warn, :error, :fatal, :unknown].include?(meth)
+        @logger.send(meth, message(args.first))
+      else
+        super
+      end
+    end
+
+    def message( msg )
+      "#{' ' * @indent_size}#{@prefix}#{msg}"
+    end
+
   end
-  
+
 end
+
+if defined?(Logger)
+  Logger.send(:include, LogBlock::InstanceMethods)
+end
+
+if defined?(ActiveSupport) and defined?(ActiveSupport::BufferedLogger)
+  ActiveSupport::BufferedLogger.send(:include, LogBlock::InstanceMethods)
+end
+
